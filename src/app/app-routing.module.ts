7ee170b64af4e91287e52d6fa5d7dc93f3595479
Alice-Githui <gnyokabi.alice@gmail.com>
2021-04-05 12:43:50 +0300

    Add the navbar, footer and homepage component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomepageComponent},
+  {path: '**', component: HomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
